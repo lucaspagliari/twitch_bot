89ff5fb9bae7eec4136d0f5a548e5535a9bee3b6
lucaspagliari <lucas.alex.pagliari@gmail.com>
2022-02-03 16:05:34 -0300

    implements guess age and guess dice

--- src/commands/random/index.ts
+++ src/commands/random/index.ts
@@ -2,7 +2,7 @@ import { Client } from "tmi.js"
 import { params } from "../controllers"
 
 
-function _dice() {
+function _dice(): number {
   return Math.floor(Math.random() * 6 + 1);
 }
 
@@ -11,17 +11,36 @@ function dice(client: Client, params: params) {
   client.say(params.target, `@${user} Your Rolled: ${_dice()}`);
 }
 
-function guessage(client: Client, params: params) {
-  if (params.args[0] == "22") {
-    client.say(params.target, `You got it right`);
+function guessdice(client: Client, params: params) {
+  if (params.args.length == 0) {
     return
   }
-  client.say(params.target, `Hmmmm wrong answer, try again later...`);
+
+  const result = _dice().toString()
+
+  const msg = `@${params.context.username} guessed ${params.args[0]}. `
+  + `Dice rolled ${result}`
+  
+  client.say(params.target, msg)
 }
 
+function guessage(client: Client, params: params) {
+  if (params.args.length == 0) {
+    return
+  }
+  const diff = new Date().getTime() - new Date(1999, 1, 13).getTime()
+  const age = Math.abs(new Date(diff).getUTCFullYear() - 1970)
+
+  if (params.args[0] == age.toString()) {
+    client.say(params.target, `@${params.context.username} You got it right!!`);
+    return
+  }
+  client.say(params.target, `Hmmmm wrong answer @${params.context.username}, try again later...`);
+}
 
 
 export default {
   dice,
-  guessage
+  guessage,
+  guessdice
 }
